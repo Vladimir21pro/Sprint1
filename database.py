0008ffd2ad6579b2ff265848454b574b9761b88d
Vladimir21pro <kutsovy@gmail.com>
2025-07-21 21:57:19 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+
+DB_HOST = os.getenv("FSTR_DB_HOST")
+DB_PORT = os.getenv("FSTR_DB_PORT")
+DB_USER = os.getenv("FSTR_DB_LOGIN")
+DB_PASS = os.getenv("FSTR_DB_PASS")
+DB_NAME = "fstr_db"
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
